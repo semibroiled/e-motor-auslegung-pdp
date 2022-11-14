236cfea97377959964f9c5fb48b12da25ec62780
semibroiled <amitavcmostafa@gmx.de>
2022-11-14 22:19:30 +0100

    Made approach chaneg to code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 import random
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import time
 import pytest
 
@@ -14,114 +14,185 @@ the necessary parameters for Energy Consumption in an E-Auto'''
 
 #Experimental constants and coefficients from table    
     
-rho = { 'air1': 1.27554, #Air density with water load at
+density = { 'air1': 1.27554, #Air density with water load at
         'air2': None #Air density IUPAC
     }
 
-c_rr = {'pavement':1.05, #block shape
+road_roll_coeff = {'pavement':1.05, #block shape
             }
+
+drag_coeff = {'placeholder' : 1.23}
+
+#Vehicle Parameters
+
+cars = {'Weight': 2200,
+        'Maximum Speed': 30,
+        'Nominal Deployment Speed': 10,
+        'Acceleration Time to Max Speed': 5,
+        'Steepest Slope Angle': 10,
+        'Typical Slope Angle':0,
+        'Car Height': 1.5,
+        'Car Width': 1.5}
 
 
 
 #Calculate the Interial Force of car
-def resistive_inertia(v_car=10, t_end=5, t_init =0, v_init = 0, m_car = 2200):
-    '''Insert misc'''
+def resistive_inertia(m_car, v_car_max, v_car_nom, t_end, t_init =0, v_init = 0):
+    '''
+    Inputs are: 
+    
+    m_car as mass of car or vehicle in kg,
+    v_car_max as the maximum car velocity in ms^-1,
+    v_car_nom as the nominal car velocity in ms^-1
+    t_end as accelertion time take to prescribed speed in s. 
+
+    Other Inputs are:
+
+    t_init as initial time in s and
+    v_init as initial speed in ms^-1
+
+    Both of which are by default 0
+
+    Output is the resistive Inertia of vehicle f_inertia_max and f_inertia_nom for both use cases in N 
+    
+    '''
     #Check input types
-    #
-    #
-    user_input = input('Do you want to calculate with top speed? Press Enter to skip and calculate at Operational Speed. Enter YES to calculate at Topspeed: \n')
 
-
-    if user_input:
-        try:
-            v_car_top = 30 #Cars topspeed in kmh
-            print(f'Car\'s Topspeed is {v_car_top} kmh')
-            v_car_top_in_ms = v_car_top / 3.6 #Cars topspeed in ms
-            a_car = ( (v_car_top_in_ms - v_init) / (t_end - t_init) )
-        except ValueError as ve:
-            print(ve)
-    else:
-         #vehicle_speed = int(input(f'Give the speed of your car travelling in kmh'))*3.6 #Cars travelling speed, usually 10kmh
-         #time_frame =  int(input(f'Give the time required for your car travelling to accelerate to topspeed in s')) #Cars time required to accelerate
-         v_car_operational_in_kmh = v_car
-         v_car_operational_in_ms = v_car_operational_in_kmh/3.6
-         print(f'Car\'s speed at normal operation is {v_car_operational_in_kmh} kmh')
-         a_car = ( (v_car_operational_in_ms - v_init) / (t_end - t_init) )
-
-    f_inertia = m_car * a_car
-    return f_inertia
-
-#Calculate the forces acting on car on a slope
-def resistive_gradient (alpha_s=10, g=9.81, m_car=2200):
-    '''Insert Description here'''
-    #Check types here
-    #
-    #
+    #Convert Inputs to suitable calculable units
     try:
-        f_s = abs(m_car * g * np.sin(alpha_s))
+        print(f'Car\'s Topspeed is {v_car_max} kmh')
+        v_car_max_in_ms = v_car_max / 3.6 #Cars topspeed in ms^-1
+        v_car_nom_in_ms = v_car_nom / 3.6 #Cars nominal speed in ms^-1
     except ValueError as ve:
         print(ve)
-    return f_s
+    
+    #Calculate with converted values
+    try:
+        a_car_max_in_ms2 =  (v_car_max_in_ms - v_init) / (t_end - t_init) #max accelertaion of car in ms^-2
+        a_car_nom_in_ms2 = (v_car_nom_in_ms - v_init) / (t_end - t_init) #nominal acceleration of car in ms^-2
+        f_inertia_max = m_car * a_car_max_in_ms2 #Inertial force in N
+        f_inertia_nom = m_car * a_car_nom_in_ms2 #Inertial force in N
+    except:
+        pass
+
+    return f_inertia_max, f_inertia_nom
+
+
+
+#Calculate the forces acting on car on a slope
+def resistive_gradient (m_car, alpha_s_max, alpha_s_nom, g=9.81):
+    '''
+    Inputs are: 
+    
+    m_car as mass of car or vehicle in kg,
+    alpha_s_max as the maximum road steepness in degrees and
+    alpha_s_nom as the typical road steepness in degrees.
+    
+
+    Other Inputs are:
+
+    g as gravitational acceleration in ms^-2 with a default value of 9,8 
+
+    Output is the resistive Slope forces acting on vehicle f_slope_max and f_slope_nom for both use cases in N 
+    '''
+    #Check input types
+
+    #Calculate formulas
+    try:
+        print(f'Steepest road angle is {alpha_s_max}')
+        f_slope_max = abs(m_car * g * np.sin(alpha_s_max)) #Maximum force acting on vehicle due to slope in N
+        f_slope_nom = abs(m_car * g * np.sin(alpha_s_nom)) #Nominal force acting on vehicle due to slope in N
+    except ValueError as ve:
+        print(ve)
+
+    return f_slope_max, f_slope_nom
+
+
 
 #Calculation frictional losses of car
-def resistive_friction(alpha_s=10, c_rr=1, g=9.81,m=2200):
-    '''This function will calculate the frictional force acting on a car
+def resistive_friction(m_car, alpha_s_max, alpha_s_nom, c_rr, g=9.81):
+    '''
+    Inputs are: 
     
-    >The inputs are:
+    m_car as mass of car or vehicle in kg,
+    alpha_s_max as the maximum road steepness in degrees,
+    alpha_s_nom as the typical road steepness in degrees and
+    c_rr as the road rolling coefficient which is dimensionless.
+
+    Other Inputs are:
+
+    g as gravitational acceleration in ms^-2 with a default value of 9,8 
+
+    Outputs are the f_rollfriction_max and f_rollfriction_nom in N
+
+    '''
+    try:
+        f_rollfriction_max = abs(m_car * g * c_rr * np.cos(alpha_s_max))
+        f_rollfriction_nom = abs(m_car * g * c_rr * np.cos(alpha_s_nom))
+    except:
+        pass
     
-    ---> alpha_s in [-] gives the slope of road to horizontal
+    return f_rollfriction_max, f_rollfriction_nom
+
+
+
+#Calculate the air drag acting on car
+def resistive_drag(rho, c_d, width_car, height_car, v_wind, v_car_max, v_car_nom):
+    '''
+    Inputs are: 
     
-    ---> c_rr in [-] gives the road rolling resistance coefficient
+    m_car as mass of car or vehicle in kg,
+    v_car_max as the maximum car velocity in ms^-1,
+    v_car_nom as the nominal car velocity in ms^-1
+    t_end as accelertion time take to prescribed speed in s. 
+
+    Other Inputs are:
+
+    t_init as initial time in s and
+    v_init as initial speed in ms^-1
+
+    Both of which are by default 0
+
+    Output is the resistive Inertia of vehicle f_inertia_max and f_inertia_nom for both use cases in N 
     
-    ---> g in [ms^-2] gives the gravitional acceleration
-    The deafault value of g is 9.81 ms^-2
+    '''
+    try:
+        v_car_max_in_ms = 30/3,6
+        v_car_nom_in_ms = 10/3,6
+    except:
+        pass
+        
+    try:
+        f_drag_max = 0.5 * rho * c_d * width_car * height_car * ((v_car_max_in_ms)**2)
+        f_drag_nom = 0.5 * rho * c_d * width_car * height_car * ((v_car_nom_in_ms)**2)
+    except ValueError as ve:
+        print(ve)
     
-    ---> m_car in [kg] gives the mass of the vehicle.
-    The default value is 2200 kg.
+    return f_drag_max, f_drag_nom
+
+
+
+
+
+
+#Calculate the Total Road Force acting on the car
+def total_resistive_force(f_inertia, f_friction, f_drag, f_slope):
 
     '''
     
-    f_r = abs(m * g * c_rr * np.cos(alpha_s))
-
-    return f_r
-
-#Calculate the air drag acting on car
-def resistive_drag(fluid_density='air1', c_d='placeholder1', frontal_area = 1.5**2, v_wind=0, v_car = 10):
-    '''Insert misc'''
-
-    air_density = rho[fluid_density]
-    c_d = 1
-    user_input = input(f'Do you want to calculate at nominal wind speed of 0kmh? Press Enter or enter Yes to get speed prompt')
-    if user_input:
-
-        wind_speed_in_ms= int(input(f'The wind speed in kmh:\n'))*3.6 #wind speed in ms
-        v_wind = wind_speed_in_ms.copy()
+    '''
+    #eff_f_t_start = time.perf_counter()
+    f_total_max = f_inertia+ f_slope+ f_friction+ f_drag #Total Road Force f_t in N
+    f_total_nom = f_inertia + f_slope + f_friction + f_drag #Toal Road Force f_t in N
     
-    else:
-        pass
-
-    user_input = input('Do you want to calculate drag at top speed? Enter to skip and calculate at 10kmh')
-    if user_input:
-        try:
-            v_car_top = 30
-            f_a = 0.5 * air_density * c_d * frontal_area * ((v_car_top-v_wind)**2)
-        except ValueError as ve:
-            print(ve)
-    else:
-        f_a = 0.5 * air_density * c_d * frontal_area * ((v_car-wind_speed)**2)
     
-    return f_a
-
-#Calculate the Total Road Force acting on the car
-def total_resistive_force(f_inertia, f_friction, f_wind, f_slope = 0):
-    eff_f_t_start = time.perf_counter()
-    f_t = f_inertia+ f_slope+ f_friction+ f_wind #Total Road Force f_t in N
-    return f_t
+    return f_total_max, f_total_nom
 
 
 
 if __name__ == '__main__':
-    i = 2 #Zeitverzog
+    i = 2 #Time Delay
+    j = time.sleep(2) #Time Delay Practice
     print('Run script...')
     time.sleep(i)
     
